8460850d16df282c3d525404db3a7d68ce5ee2be
Shayaan Emran <shayaan.emran@gmail.com>
2023-09-07 22:05:47 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.41

---
+++ src/DroughtIndices.jl
@@ -0,0 +1,5 @@
+module DroughtIndices
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using DroughtIndices
+using Test
+
+@testset "DroughtIndices.jl" begin
+    # Write your tests here.
+end
